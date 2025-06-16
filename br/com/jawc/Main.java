396913e087fb7f622b8081003d2c5badf7809f70
jawc-05 <joaoalfredowc@gmail.com>
2025-06-16 16:27:44 -0300

    simple exercise with classes

---
+++ br/com/jawc/Main.java
@@ -0,0 +1,14 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Moto minhaMoto = new Moto("CG", "160", "Preta", 2025);
+
+        minhaMoto.showInfo();
+    }
+}
--- br/com/jawc/WhyThisBranch.java
+++
@@ -1,9 +0,0 @@
-package br.com.jawc;
-
-public class WhyThisBranch {
-
-    public static void main(String[]args){
-        System.out.println("Hello World");
-        System.out.println("Just testing IntelliJ IDEA");
-    }
-}
