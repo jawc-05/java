240041d1ec3cb2d235526dd2cfd63e13f02026aa
jawc-05 <joaoalfredowc@gmail.com>
2025-06-26 10:35:41 -0300

    wrappers

---
+++ br/com/jawc/Wrappers.java
@@ -0,0 +1,21 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc;
+
+public class Wrappers {
+
+    private Integer codigo;
+    private Long codigoMaior = 123123123123l;
+    private Double valorDecimal = 10.1;
+    private Float valorDecimal2 = 10.2f;
+    private Short shor;
+    private Byte bi;
+    private Character letra = 'A';
+    private Boolean status = false;
+
+    public  void main (String[] args) {
+        this.codigo = null;
+
+    }
+}
