11e945dfaea9397a72c0a85a2f7683a428742c92
jawc-05 <joaoalfredowc@gmail.com>
2025-06-26 12:18:54 -0300

    simples examples of casting in java

---
+++ br/com/jawc/Casting.java
@@ -0,0 +1,31 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc;
+
+public class Casting {
+
+    public static void main(String[] args) {
+        //casting explicito
+        int num1 = 10;
+        short num2 = (short) num1;
+        long numl = 111111111111111111l;
+        System.out.println(numl);
+        int num4 = (int) numl;
+        System.out.println(num4);
+        int i1 = 1;
+        byte b1 = (byte) i1;
+
+git
+
+        //Casting Implicito
+        int idade = 18;
+        long idadeL = idade;
+        byte bi = 23;
+        short biS = bi;
+
+
+
+
+    }
+}
