a3f4d4e68b31a6919df561722f080ed2d9b86e58
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-07-08 15:26:31 -0300

    simple queues

---
+++ br/com/jawc/collection/queue/ExampleArrayDeque.java
@@ -0,0 +1,10 @@
+/**
+ * @author jawc
+ */
+
+package br.com.jawc.collection.queue;
+public class ExampleArrayDeque {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ br/com/jawc/collection/queue/ExamplePrioriryQueue.java
@@ -0,0 +1,10 @@
+/**
+ * @author jawc
+ */
+
+package br.com.jawc.collection.queue;
+public class ExamplePrioriryQueue {
+    public static void main(String[] args) {
+
+    }
+}
