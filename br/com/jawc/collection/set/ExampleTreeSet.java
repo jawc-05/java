a420372791b2221221b548856c8e57f7893db568
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-08-05 16:07:49 -0300

    feat: adding TreeSet Example

---
+++ br/com/jawc/collection/set/ExampleTreeSet.java
@@ -0,0 +1,31 @@
+/**
+ * @author jawc
+ */
+
+package br.com.jawc.collection.set;
+
+import java.util.HashSet;
+import java.util.TreeSet;
+import java.util.Set;
+
+public class ExampleTreeSet {
+
+    public static void main(String[] args) {
+    exemploListaSimples();
+        /**
+         * não repete os valores na lista
+         * tem ordem natural dos objetos
+         */
+    }
+
+    public static void exemploListaSimples() {
+        System.out.println("*****exemploListaSimples");
+        Set<String> lista = new TreeSet<>();
+        lista.add("João da Silva");
+        lista.add("Antonio Sousa");
+        lista.add("Lúcia Ferreira");
+        lista.add("João da Silva");
+        System.out.println(lista);
+        System.out.println(" ");
+    }
+}
