7018ed6f7e44733fb035aaeef96c4d59c860013b
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-08-07 16:28:25 -0300

    feat: added ExampleBubbleSortDynamic.java

---
+++ br/com/jawc/array/ExampleBubbleSortDynamic.java
@@ -0,0 +1,43 @@
+/**
+ * @author jawc
+ */
+
+package br.com.jawc.array;
+
+import java.util.Scanner;
+
+public class ExampleBubbleSortDynamic {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite os números separados por , ");
+
+        String answer = sc.nextLine();
+        String[] vetStr = answer.split(",");
+        int [] vet = convert(vetStr);
+        int aux = 0;
+        int i = 0;
+
+        System.out.println("Vetor desordenado: ");
+        for(i = 0; i<vet.length; i++){
+            System.out.println(" "+vet[i]);
+        }
+        System.out.println(" ");
+
+        for(i = 0; i < vet.length; i++){
+            for(int j = 0; j<vet.length-1;j++){
+                if(vet[j] > vet[j+1]){
+                    aux = vet[j];
+                    vet[j] = vet[j+1];
+                    vet[j+1] = aux;
+                }
+            }
+        }
+        System.out.println("Vetor ordenado: ");
+        for(i = 0; i < vet.length; i++){
+            System.out.println(" " +vet[i]);
+        }
+    }
+
+
+}
