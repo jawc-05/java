9bad57db4ac858ac394442fc7ef7f129273a4809
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-08-08 15:32:18 -0300

    feat: added exemploListaSimplesIterandoChaves

--- br/com/jawc/map/ExampleHashMap.java
+++ br/com/jawc/map/ExampleHashMap.java
@@ -11,6 +11,34 @@ public class ExampleHashMap {
 
     public static void main(String[] args) {
         exemploListaSimples();
+        exemploListaSimplesIterandoValores();
+        exemploListaSimplesIterandoChaves();
+
+    }
+
+    private static void exemploListaSimplesIterandoChaves() {
+        System.out.println("***** exemploListaSimplesIterandoChaves *****");
+        Map<Integer, String> lista = new HashMap<>();
+        lista.put(1, "João da Silva");
+        lista.put(2, "Antonio Sousa");
+        lista.put(4, "Maria Sousa");
+        lista.put(3, "Lúcia Ferreira");
+
+        for (Integer value : lista.keySet()) {
+            System.out.println(value);
+        }
+    }
+
+    private static void exemploListaSimplesIterandoValores() {
+        System.out.println("***** exemploListaSimplesIterandoValores *****");
+        Map<Integer, String> lista = new HashMap<>();
+        lista.put(1, "João da Silva");
+        lista.put(2, "Antonio Sousa");
+        lista.put(4, "Maria Sousa");
+        lista.put(3, "Lúcia Ferreira");
+        for ( String value : lista.values() ) {
+            System.out.println(value);
+        }
     }
 
     private static void exemploListaSimples() {
