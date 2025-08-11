13ca3783b09c4e520b0016f5ad449ba3716e389b
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-08-11 15:11:33 -0300

    feat: added ordenaPrint method in Tarefa1.java and finalized it

--- br/com/jawc/collection/tarefa_colecoes/Tarefa1.java
+++ br/com/jawc/collection/tarefa_colecoes/Tarefa1.java
@@ -4,26 +4,28 @@
 
 package br.com.jawc.collection.tarefa_colecoes;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Tarefa1 {
 
     public static void main(String[] args) {
-
+    List<String> nomesLista = inputNomes();
+    ordenaPrint(nomesLista);
     }
-    public static void inputNomes(){
+    private static List<String> inputNomes(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite os nomes separados por virgula(,): ");
         String nomes = sc.nextLine();
 
         String[] nomesArray = nomes.split(",");
-        List<String> nomesLista = new ArrayList<>(Arrays.asList(nomesArray));
+        return new ArrayList<>(Arrays.asList(nomesArray));
     }
 
-    public static void ordenaPrint(){
-
+    private static void ordenaPrint(List<String> nomesLista){
+        Collections.sort(nomesLista);
+        System.out.println("Nomes em ordem Alfabética: ");
+        for(String nome : nomesLista){
+            System.out.println(nome);
+        }
     }
 }
