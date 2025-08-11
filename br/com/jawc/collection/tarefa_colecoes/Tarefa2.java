3fe5188cb8c19b35b7bfe955ab52c27615910650
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-08-11 16:08:08 -0300

    feat: finished Tarefa2.java

--- br/com/jawc/collection/tarefa_colecoes/Tarefa2.java
+++ br/com/jawc/collection/tarefa_colecoes/Tarefa2.java
@@ -9,12 +9,13 @@ import java.util.*;
 public class Tarefa2 {
 
     public static void main(String[] args) {
-
+        Map<String, List<String>> mapa = inputNomes();
+        print(mapa);
     }
     private static Map<String, List<String>> inputNomes(){
         Map<String, List<String>> mapa = new TreeMap<>();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite o nome e sexo(Ex: Joao-M,Ana-F: ");
+        System.out.println("Digite o nome e sexo(Ex: Joao-M,Ana-F: )");
         String linha = sc.nextLine();
 
         String[] pares = linha.split(",");
@@ -27,5 +28,14 @@ public class Tarefa2 {
         return mapa;
     }
 
-    private
-}
+    private static void print(Map<String, List<String>> mapa){
+        for (String sexo : mapa.keySet()) {
+            if (sexo.equals("M")) {
+                System.out.println("Nomes de Homens:");
+                System.out.println(mapa.get("M"));
+            } else if (sexo.equals("F")) {
+            System.out.println("Nomes de Mulheres:");
+            System.out.println(mapa.get("F"));
+        }
+    }
+}}
