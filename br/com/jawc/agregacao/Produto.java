8e2327a8d98a38da331ae6631dd53e82c3c1f4ca
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-08-12 16:32:49 -0300

    feat: added Venda.java

--- br/com/jawc/agregacao/Produto.java
+++ br/com/jawc/agregacao/Produto.java
@@ -6,6 +6,24 @@ package br.com.jawc.agregacao;
 public class Produto {
 
     private Double preco;
+    private String nome;
+    private Long codigo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
 
     public Double getPreco() {
         return preco;
---
+++ br/com/jawc/agregacao/Venda.java
@@ -0,0 +1,50 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.agregacao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Venda {
+
+    private  Comprador comprador;
+    private Vendedor vendedor;
+    private List<Produto> produtos;
+
+    public Venda(){
+        this.produtos = new ArrayList<>();
+    }
+
+    public Comprador getComprador() {
+        return comprador;
+    }
+
+    public void setComprador(Comprador comprador) {
+        this.comprador = comprador;
+    }
+
+    public Vendedor getVendedor() {
+        return vendedor;
+    }
+
+    public void setVendedor(Vendedor vendedor) {
+        this.vendedor = vendedor;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void add (Produto produto){
+        this.produtos.add(produto);
+    }
+
+    public void concretizarVenda(){
+
+    }
+
+    public void cancelarVenda(){
+
+    }
+}
