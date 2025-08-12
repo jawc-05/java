3404c015b8bbc0f583736e81f0bfb587db342dfe
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-08-12 14:44:35 -0300

    feat: added default method in intefarce(just works in java 8 to forward)

--- br/com/jawc/interfaces/ICaneta.java
+++ br/com/jawc/interfaces/ICaneta.java
@@ -5,6 +5,11 @@ package br.com.jawc.interfaces;
 
 public interface ICaneta {
 
+    default void escreverComumATodas(){
+        System.out.println("Escrita por meio default");
+    }
+
+
     public void escrever(String texto);
 
     public String getCor();
--- br/com/jawc/interfaces/Main.java
+++ br/com/jawc/interfaces/Main.java
@@ -8,14 +8,17 @@ public class Main {
     public static void main(String[] args) {
          ICaneta caneta = new CanetaEsferografica();
          caneta.escrever("Olá João");
+         caneta.escreverComumATodas();
          System.out.println(caneta.getCor());
 
         ICaneta giz = new Giz();
         giz.escrever("Olá João");
+        giz.escreverComumATodas();
         System.out.println(giz.getCor());
 
         ICaneta lapis = new Lapis();
         lapis.escrever("Olá João");
+        lapis.escreverComumATodas();
         System.out.println(lapis.getCor());
     }
 }
