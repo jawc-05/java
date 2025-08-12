00da6e435dabd2b9a5deca689e469eca06a5fc1c
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-08-12 15:03:51 -0300

    feat: added abstratas package and Empregado.java

---
+++ br/com/jawc/abstratas/Empregado.java
@@ -0,0 +1,12 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.abstratas;
+
+public  abstract class Empregado {
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+
+
+}
