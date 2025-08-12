4528a341d896c14fd01c74297317781a14532fe2
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-08-12 14:31:48 -0300

    feat: added CanetaEsferografica.java

---
+++ br/com/jawc/interfaces/CanetaEsferografica.java
@@ -0,0 +1,17 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.interfaces;
+
+public class CanetaEsferografica  implements ICaneta{
+
+    @Override
+    public void escrever(String texto) {
+        System.out.println("Escrevendo o valor " + texto + " na classe " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getCor() {
+        return "Preta";
+    }
+}
