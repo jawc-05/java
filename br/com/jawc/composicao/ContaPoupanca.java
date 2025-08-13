65ebb0bc8cfa7e0c736c83b95457a32e57c2b2a5
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-08-13 14:39:59 -0300

    feat: added getters and setter in ContaPoupanca

--- br/com/jawc/composicao/ContaPoupanca.java
+++ br/com/jawc/composicao/ContaPoupanca.java
@@ -4,4 +4,14 @@
 package br.com.jawc.composicao;
 
 public class ContaPoupanca {
+
+    private Double saldo;
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
 }
