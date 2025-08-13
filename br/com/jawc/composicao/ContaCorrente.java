39a3104bf718cb5ccaa3b9efbc39d6668ec45b15
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-08-13 14:39:23 -0300

    feat: added getters and setter in ContaCorrente

--- br/com/jawc/composicao/ContaCorrente.java
+++ br/com/jawc/composicao/ContaCorrente.java
@@ -4,4 +4,23 @@
 package br.com.jawc.composicao;
 
 public class ContaCorrente {
+
+    private Double saldo;
+    private  Double chequeEspecial;
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Double getChequeEspecial() {
+        return chequeEspecial;
+    }
+
+    public void setChequeEspecial(Double chequeEspecial) {
+        this.chequeEspecial = chequeEspecial;
+    }
 }
