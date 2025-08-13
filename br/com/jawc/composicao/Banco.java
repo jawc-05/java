1136bc25dcb752833309a18283f2576a4738f8cc
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-08-13 14:38:07 -0300

    feat: added methods in Banco

--- br/com/jawc/composicao/Banco.java
+++ br/com/jawc/composicao/Banco.java
@@ -3,5 +3,24 @@
  */
 package br.com.jawc.composicao;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Banco {
+
+    private Set<ContaCorrente> contasCorrentes;
+    private Set<ContaPoupanca> contasPoupances;
+
+    public Banco() {
+        this.contasCorrentes = new HashSet<>();
+        this.contasPoupances = new HashSet<>();
+    }
+
+    public void add(ContaCorrente cc) {
+        this.contasCorrentes.add(cc);
+    }
+
+    public void add(ContaPoupanca cp) {
+        this.contasPoupances.add(cp);
+    }
 }
