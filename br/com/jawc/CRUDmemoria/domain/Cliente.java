60132a8fa049aad3b235c08e87199ab3b27c4d30
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-08-15 14:17:57 -0300

    feat: added Cliente.java in domain

---
+++ br/com/jawc/CRUDmemoria/domain/Cliente.java
@@ -0,0 +1,53 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.CRUDmemoria.domain;
+
+import java.util.Objects;
+
+public class Cliente {
+
+    private String nome;
+    private Long cpf;
+    private Long tel;
+    private String end;
+    private Integer numero;
+    private String cidade;
+    private String estado;
+
+    public Cliente(String nome, String cpf, String tel, String end, String num, String cidade, String estado) {
+        this.nome = nome;
+        this.cpf = Long.valueOf(cpf.trim());
+        this.tel = Long.valueOf(tel.trim());
+        this.end = end;
+        this.numero = Integer.valueOf(num.trim());
+        this.cidade = cidade;
+        this.estado = estado;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj){
+            return true;
+        }
+        if (obj == null){
+            return false;
+        }
+        if (getClass() != obj.getClass()){
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        if (!Objects.equals(this.cpf, other.cpf)){
+            return false;
+        }
+        return  true;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nome='" + nome + '\'' +
+                ", cpf=" + cpf +
+                '}';
+    }
+}
