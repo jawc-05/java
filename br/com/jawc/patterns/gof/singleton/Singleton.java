83e965c3a397652f253a2400a42216e35b62b22c
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-08-29 15:25:46 -0300

    feat: adding singleton

--- br/com/jawc/patterns/gof/singleton/Singleton.java
+++ br/com/jawc/patterns/gof/singleton/Singleton.java
@@ -4,4 +4,17 @@
 package br.com.jawc.patterns.gof.singleton;
 
 public class Singleton {
+
+    private Singleton singleton;
+
+    private Singleton() {
+
+    }
+
+    public Singleton getInstance() {
+        if (this.singleton == null) {
+            this.singleton = new Singleton();
+        }
+        return this.singleton;
+    }
 }
