9c1e81d3540939f9fd59e8442d0f7210641fc4ee
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-08-29 15:29:37 -0300

    feat: added demo for singleton

--- br/com/jawc/patterns/gof/singleton/DemoSingleton.java
+++ br/com/jawc/patterns/gof/singleton/DemoSingleton.java
@@ -7,5 +7,8 @@ public class DemoSingleton {
 
     public static void main(String[] args) {
         Singleton singleton = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+        System.out.println(singleton);
+        System.out.println(singleton2);
     }
 }
