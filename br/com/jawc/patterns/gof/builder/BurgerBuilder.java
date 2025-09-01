e0dbe99a061c1caef9ea8a7a7bda3a1a3078d500
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-09-01 15:22:52 -0300

    feat: addded BurgerBuilder.java

---
+++ br/com/jawc/patterns/gof/builder/BurgerBuilder.java
@@ -0,0 +1,18 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.patterns.gof.builder;
+
+public abstract class BurgerBuilder {
+
+
+    Burger burger = new Burger();
+    abstract void buildBun();
+    abstract void buildMeat();
+    abstract void buildSalad();
+    abstract void buildCheese();
+    abstract void buildSauce();
+    Burger build(){
+        return burger;
+    };
+}
