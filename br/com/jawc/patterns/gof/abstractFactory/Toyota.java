56b9e9bde287097be25ded3f06d52a52f219be62
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-09-02 15:20:24 -0300

    feat: adding Toyota

---
+++ br/com/jawc/patterns/gof/abstractFactory/Toyota.java
@@ -0,0 +1,11 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.patterns.gof.abstractFactory;
+
+public class Toyota extends Car{
+
+    public Toyota(int horsePower, String fuelSource, String color) {
+        super(horsePower, fuelSource, color);
+    }
+}
