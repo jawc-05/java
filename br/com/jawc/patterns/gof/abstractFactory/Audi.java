5e6f5f8b56edc5811e71f9bb16e216bdf48ae183
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-09-02 15:20:52 -0300

    feat: adding Audi

---
+++ br/com/jawc/patterns/gof/abstractFactory/Audi.java
@@ -0,0 +1,11 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.patterns.gof.abstractFactory;
+
+public class Audi extends Car{
+
+    public Audi(int horsePower, String fuelSource, String color) {
+        super(horsePower, fuelSource, color);
+    }
+}
