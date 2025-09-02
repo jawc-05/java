eb02ebbc3f27c1117a507a888eb1d29dbab97418
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-09-02 15:02:28 -0300

    feat: adding Tesla

---
+++ br/com/jawc/patterns/gof/abstractFactory/Tesla.java
@@ -0,0 +1,11 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.patterns.gof.abstractFactory;
+
+public class Tesla extends Car{
+
+    public Tesla(int horsePower, String fuelSource, String color){
+        super(horsePower, fuelSource, color);
+    }
+}
