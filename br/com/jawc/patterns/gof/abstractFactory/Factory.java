01129fa5018c6679b167319e31bbe5a22e7a33af
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-09-02 14:53:49 -0300

    feat: adding Factory

--- br/com/jawc/patterns/gof/abstractFactory/Factory.java
+++ br/com/jawc/patterns/gof/abstractFactory/Factory.java
@@ -3,5 +3,18 @@
  */
 package br.com.jawc.patterns.gof.abstractFactory;
 
-public class Factory {
+public abstract class Factory {
+
+    public Car create(String requestGrade){
+        Car car = retrieveCar(requestGrade);
+        car = prepareCar(car);
+        return car;
+    }
+    private Car prepareCar(Car car){
+        car.clean();
+        car.mechanicCheck();
+        car.fuelCar();
+        return car;
+    }
+    abstract Car retrieveCar(String requestGrade);
 }
