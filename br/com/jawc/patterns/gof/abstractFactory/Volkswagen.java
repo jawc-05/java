f404f17c340d3742e29c0bc695677635821a9873
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-09-02 15:15:43 -0300

    feat: adding Volkswagen

---
+++ br/com/jawc/patterns/gof/abstractFactory/Volkswagen.java
@@ -0,0 +1,10 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.patterns.gof.abstractFactory;
+
+public class Volkswagen  extends Car{
+    public Volkswagen(int horsePower, String fuelSource, String color){
+        super(horsePower, fuelSource, color);
+    }
+}
