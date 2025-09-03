8aeb53b75c1f6a8400f402118755509e7351cd90
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-09-03 08:43:02 -0300

    fix: fixing demo

--- br/com/jawc/patterns/gof/template/Demo.java
+++ br/com/jawc/patterns/gof/template/Demo.java
@@ -7,7 +7,7 @@ package br.com.jawc.patterns.gof.template;
 public class Demo {
 
     public static void main(String[] args) {
-        Worker worker = new Worker() {
-        }
+        Worker bombeiro = new Firefighter();
+        bombeiro.executeRoutine();
     }
 }
