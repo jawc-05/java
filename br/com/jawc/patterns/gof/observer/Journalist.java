d71906b88695206e3930a4612747bb8a89f256e8
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-09-03 09:24:15 -0300

    feat: adding journalist

---
+++ br/com/jawc/patterns/gof/observer/Journalist.java
@@ -0,0 +1,57 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.patterns.gof.observer;
+
+import java.util.ArrayList;
+
+public class Journalist implements Observer {
+
+    private ArrayList<Observer> observers = new ArrayList<>();
+    private String title, summary, image, text;
+
+    public ArrayList<Observer> getObservers() {
+        return observers;
+    }
+
+    public void setObservers(ArrayList<Observer> observers) {
+        this.observers = observers;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public void update(Subject subject) {
+
+    }
+}
