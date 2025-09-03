637f0044873837f4ffb2dde1ddea7aa4f2c53675
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-09-03 08:40:16 -0300

    feat: adding Demo

---
+++ br/com/jawc/patterns/gof/template/Demo.java
@@ -0,0 +1,13 @@
+/**
+ * @author jawc
+ */
+
+package br.com.jawc.patterns.gof.template;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        Worker worker = new Worker() {
+        }
+    }
+}
