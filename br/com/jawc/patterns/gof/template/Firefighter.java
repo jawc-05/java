9aaea3208094304309654b4c8c79f2cf252ed0ce
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-09-03 08:48:21 -0300

    feat: adding working method in firefighter

--- br/com/jawc/patterns/gof/template/Firefighter.java
+++ br/com/jawc/patterns/gof/template/Firefighter.java
@@ -5,4 +5,8 @@ package br.com.jawc.patterns.gof.template;
 
 public class Firefighter extends Worker {
 
+    @Override
+    protected void working() {
+        System.out.println("Firefighter working");
+    }
 }
