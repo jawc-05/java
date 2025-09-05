3f65209a97313a8f50ccad3c8302f459db9ff851
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-09-05 16:35:04 -0300

    feat: adding google drive

---
+++ br/com/jawc/patterns/gof/adapter/GoogleDrive.java
@@ -0,0 +1,14 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.patterns.gof.adapter;
+
+public class GoogleDrive {
+
+    void send(byte[] data, String name){
+
+    }
+    byte[] get(String name){
+        return null;
+    }
+}
