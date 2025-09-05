acc7e315bce372d6ef3a4f4ef1b9593e2b210be0
Joao Alfredo Williges Cunha <joaoalfredowc@gmail.com>
2025-09-05 16:37:25 -0300

    feat: adding methods in dropbox

--- br/com/jawc/patterns/gof/adapter/DropBox.java
+++ br/com/jawc/patterns/gof/adapter/DropBox.java
@@ -4,4 +4,12 @@
 package br.com.jawc.patterns.gof.adapter;
 
 public class DropBox {
+
+    void upload(DropBoxFile dbFile){
+
+    }
+
+    DropBoxFile download(String id){
+        return null;
+    }
 }
