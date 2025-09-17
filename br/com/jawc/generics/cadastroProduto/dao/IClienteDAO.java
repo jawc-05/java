c835a7771bd5946c40f418250f131b38f64bdfdc
jawc-05 <joaoalfredowc@gmail.com>
2025-09-17 14:57:23 -0300

    feat: adding dao and interface Cliente

---
+++ br/com/jawc/generics/cadastroProduto/dao/IClienteDAO.java
@@ -0,0 +1,17 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.generics.cadastroProduto.dao;
+
+import br.com.jawc.generics.cadastroProduto.domain.Cliente;
+
+import java.util.Collection;
+
+public interface IClienteDAO {
+
+    public boolean cadastrar(Cliente cliente);
+    public void excluir(Long cpf);
+    public void alterar(Cliente cliente);
+    public Cliente buscar(Long cpf);
+    public Collection<Cliente> listar();
+}
