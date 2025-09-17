fa1feee61a94e7c92e564bf5250e1041358d13da
jawc-05 <joaoalfredowc@gmail.com>
2025-09-17 11:06:41 -0300

    feat: adding methods to data

--- br/com/jawc/generics/GenericEntry.java
+++ br/com/jawc/generics/GenericEntry.java
@@ -3,4 +3,12 @@ package br.com.jawc.generics;
 public class GenericEntry<T> {
 
     private T data;
+
+    public GenericEntry(T data) {
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
 }
