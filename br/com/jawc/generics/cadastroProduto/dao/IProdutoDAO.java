1b66e1eada09b5a43301481c130b4403dd42926c
jawc-05 <joaoalfredowc@gmail.com>
2025-09-17 15:10:42 -0300

    feat: adding methods to the interface

--- br/com/jawc/generics/cadastroProduto/dao/IProdutoDAO.java
+++ br/com/jawc/generics/cadastroProduto/dao/IProdutoDAO.java
@@ -3,5 +3,19 @@
  */
 package br.com.jawc.generics.cadastroProduto.dao;
 
+import br.com.jawc.generics.cadastroProduto.domain.Produto;
+
+import java.util.Collection;
+
 public interface IProdutoDAO {
+
+    public Boolean cadastrar(Produto produto);
+
+    public void excluir(Long codigo);
+
+    public void alterar(Produto produto);
+
+    public Produto consultar(Long codigo);
+
+    public Collection<Produto> consultarTodos();
 }
