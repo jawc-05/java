8542117f0af4105e44191ac4819658dfe74a331c
jawc-05 <joaoalfredowc@gmail.com>
2025-09-17 15:05:30 -0300

    feat: adding ClienteMapDAO

---
+++ br/com/jawc/generics/cadastroProduto/dao/ClienteMapDAO.java
@@ -0,0 +1,62 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.generics.cadastroProduto.dao;
+
+import br.com.jawc.generics.cadastroProduto.domain.Cliente;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ClienteMapDAO implements IClienteDAO{
+
+    private Map<Long, Cliente> map;
+
+    public ClienteMapDAO() {
+        this.map = new HashMap<Long, Cliente>();
+    }
+
+    @Override
+    public Boolean cadastrar(Cliente cliente) {
+        if (this.map.containsKey(cliente.getCpf())) {
+            return false;
+        }
+        this.map.put(cliente.getCpf(), cliente);
+        return true;
+    }
+
+    @Override
+    public void excluir(Long cpf) {
+        Cliente clienteCadastrado = this.map.get(cpf);
+
+        if (clienteCadastrado != null) {
+            this.map.remove(clienteCadastrado.getCpf(),  clienteCadastrado);
+        }
+    }
+
+    @Override
+    public void alterar(Cliente cliente) {
+        Cliente clienteCadastrado = this.map.get(cliente.getCpf());
+
+        if (clienteCadastrado != null) {
+            clienteCadastrado.setNome(cliente.getNome());
+            clienteCadastrado.setCpf(cliente.getCpf());
+            clienteCadastrado.setTel(cliente.getTel());
+            clienteCadastrado.setNumero(cliente.getNumero());
+            clienteCadastrado.setCidade(cliente.getCidade());
+            clienteCadastrado.setEstado(cliente.getEstado());
+            clienteCadastrado.setEnd(cliente.getEnd());
+        }
+    }
+
+    @Override
+    public Cliente buscar(Long cpf) {
+        return this.map.get(cpf);
+    }
+
+    @Override
+    public Collection<Cliente> listar() {
+        return this.map.values();
+    }
+}
