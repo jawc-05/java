75453c2fed6a4cdeae2666a154a4eda4c708c3f8
jawc-05 <joaoalfredowc@gmail.com>
2025-09-19 15:39:24 -0300

    feat: adding ProdutoDAO constructor and the correct extension

--- br/com/jawc/generics/cadastroProduto/dao/ProdutoDAO.java
+++ br/com/jawc/generics/cadastroProduto/dao/ProdutoDAO.java
@@ -3,11 +3,31 @@
  */
 package br.com.jawc.generics.cadastroProduto.dao;
 
+import br.com.jawc.generics.cadastroProduto.dao.generic.GenericDAO;
+import br.com.jawc.generics.cadastroProduto.domain.Cliente;
 import br.com.jawc.generics.cadastroProduto.domain.Produto;
 
 import java.util.Collection;
 import java.util.List;
 
-public class ProdutoDAO implements IProdutoDAO {
+public class ProdutoDAO extends GenericDAO<Produto> implements IProdutoDAO {
 
+    public ProdutoDAO() {
+        super();
+    }
+
+    @Override
+    public void atualizarDados(Produto entity, Produto entityCadastrado) {
+
+    }
+
+    @Override
+    public void atualizarDados(Cliente entity) {
+
+    }
+
+    @Override
+    public Class<Produto> getClassType() {
+        return null;
+    }
 }
