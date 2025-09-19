d0ffb20e84b36ce513ceb05518fe34b0d44193c8
jawc-05 <joaoalfredowc@gmail.com>
2025-09-19 15:37:30 -0300

    fix: removed methods in ProdutoDAO

--- br/com/jawc/generics/cadastroProduto/dao/ProdutoDAO.java
+++ br/com/jawc/generics/cadastroProduto/dao/ProdutoDAO.java
@@ -9,28 +9,5 @@ import java.util.Collection;
 import java.util.List;
 
 public class ProdutoDAO implements IProdutoDAO {
-    @Override
-    public Boolean cadastrar(Produto produto) {
-        return null;
-    }
 
-    @Override
-    public void excluir(Long codigo) {
-
-    }
-
-    @Override
-    public void alterar(Produto produto) {
-
-    }
-
-    @Override
-    public Produto consultar(Long codigo) {
-        return null;
-    }
-
-    @Override
-    public Collection<Produto> consultarTodos() {
-        return List.of();
-    }
 }
