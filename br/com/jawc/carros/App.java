c3fc5aea17c7ecb8aa3c6292d8023eac9c7d6421
jawc-05 <joaoalfredowc@gmail.com>
2025-10-01 15:04:50 -0300

    feat: adding List<Carro> and paramenters

--- br/com/jawc/carros/App.java
+++ br/com/jawc/carros/App.java
@@ -10,5 +10,9 @@ public class App {
 
     public static void main(String[] args) {
         List<Carro> carros = new ArrayList<Carro>();
+        carros.add(new Civic("Honda", "Type-R", 2025));
+        carros.add(new Corolla("Toyota", "GR", 2025));
+        carros.add(new Jetta("Volkswagen", "TSI", 2025));
+        carros.add(new Fusion("Ford", "Titanium", 2025));
     }
 }
