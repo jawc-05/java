3ae88d3b6970ec68beccc1695acaf3e818dfaf82
jawc-05 <joaoalfredowc@gmail.com>
2025-10-01 15:14:50 -0300

    feat: adding getters and setter in Carro

--- br/com/jawc/carros/Carro.java
+++ br/com/jawc/carros/Carro.java
@@ -16,4 +16,28 @@ public abstract class Carro {
         this.modelo = modelo;
         this.ano = ano;
     }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public Integer getAno() {
+        return ano;
+    }
+
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
 }
