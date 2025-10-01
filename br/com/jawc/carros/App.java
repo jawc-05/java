0791f28972fb7cc554cd743ea421c67704994b26
jawc-05 <joaoalfredowc@gmail.com>
2025-10-01 14:58:34 -0300

    feat: adding App

---
+++ br/com/jawc/carros/App.java
@@ -0,0 +1,14 @@
+/**
+ * @author jawc-05
+ */
+package br.com.jawc.carros;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+
+    public static void main(String[] args) {
+        List<Carro> carros = new ArrayList<Carro>();
+    }
+}
