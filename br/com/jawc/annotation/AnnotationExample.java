f05424e8e68179ed2e76f0da8c1e3bfe34f91c0a
jawc-05 <joaoalfredowc@gmail.com>
2025-10-22 16:44:51 -0300

    feat: adding long

--- br/com/jawc/annotation/AnnotationExample.java
+++ br/com/jawc/annotation/AnnotationExample.java
@@ -12,4 +12,6 @@ public @interface AnnotationExample {
     String value();
 
     String[] bairros();
+
+    long numCasa();
 }
--- br/com/jawc/annotation/ClassWithAnnotation.java
+++ br/com/jawc/annotation/ClassWithAnnotation.java
@@ -3,9 +3,9 @@
  */
 package br.com.jawc.annotation;
 
-@AnnotationExample(value = "João", bairros = "CEE")
+@AnnotationExample(value = "João", bairros = "CEE", numCasa = 55)
 public class ClassWithAnnotation {
 
-    @AnnotationExample(value = "Alfredo", bairros = {"Cee", "Cruzeiro"})
+    @AnnotationExample(value = "Alfredo", bairros = {"Cee", "Cruzeiro"}, numCasa = 105)
     private String nome;
 }
