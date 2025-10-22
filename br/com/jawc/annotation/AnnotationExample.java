94c8f16e49f05c8fe77ea1f4128f1d831dba98ae
jawc-05 <joaoalfredowc@gmail.com>
2025-10-22 16:41:02 -0300

    feat: adding ClassWithAnnotation

--- br/com/jawc/annotation/AnnotationExample.java
+++ br/com/jawc/annotation/AnnotationExample.java
@@ -7,6 +7,6 @@ import java.lang.annotation.*;
 
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.FIELD)
+@Target({ElementType.TYPE, ElementType.FIELD})
 public @interface AnnotationExample {
 }
---
+++ br/com/jawc/annotation/ClassWithAnnotation.java
@@ -0,0 +1,11 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.annotation;
+
+@AnnotationExample
+public class ClassWithAnnotation {
+
+    @AnnotationExample
+    private String nome;
+}
