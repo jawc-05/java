9665da2905255b019c088096b920a0be4a8edf8c
jawc-05 <joaoalfredowc@gmail.com>
2025-10-26 18:06:37 -0300

    feat: addinh saque

--- br/com/jawc/exceptions/ExampleExceptionBasic.java
+++ br/com/jawc/exceptions/ExampleExceptionBasic.java
@@ -27,4 +27,15 @@ public class ExampleExceptionBasic {
         System.out.println("Frase Antiga: "+ phrase);
         System.out.println("Frase Nova: "+ newPhrase);
     }
+
+    private static void saque (double valor) throws LimiteSaqueException{
+
+        if (valor > 400){
+            LimiteSaqueException erro =
+                    new LimiteSaqueException("O valor solicitado passa o limite diário que pode ser sacado nessa conta");
+            throw erro;
+        }else{
+            System.out.println("Saldo retirado da conta: "+valor+"$");
+        }
+    }
 }
