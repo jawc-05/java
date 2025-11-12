4388987d2b7bcfb8b91a767026f4c574ead62cfc
jawc-05 <joaoalfredowc@gmail.com>
2025-11-12 14:59:37 -0300

    feat: adding AppExampleThrow

---
+++ br/com/jawc/exceptions/AppExampleThrow.java
@@ -0,0 +1,15 @@
+/**
+ * @author jawc-05
+ */
+package br.com.jawc.exceptions;
+
+public class AppExampleThrow {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static void runtimeException(){
+        Exemplo.throw
+    }
+}
