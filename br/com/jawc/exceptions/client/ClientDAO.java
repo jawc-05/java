8122ff3ed8ed40d2960dc3cbdbd3c32992d7c0be
jawc-05 <joaoalfredowc@gmail.com>
2025-11-12 15:46:32 -0300

    feat: adding ClientDAO

---
+++ br/com/jawc/exceptions/client/ClientDAO.java
@@ -0,0 +1,16 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.exceptions.client;
+
+public class ClientDAO {
+
+    public static void searchClient(String code) throws ClientNotFoundException {
+        //BUSCA NO BANCO DE DADOS
+        boolean isSigned = false;
+
+        if (!isSigned) {
+            throw new ClientNotFoundException("Cliente não foi encontrado!");
+        }
+    }
+}
--- br/com/jawc/exceptions/client/ClientService.java
+++ br/com/jawc/exceptions/client/ClientService.java
@@ -6,11 +6,6 @@ package br.com.jawc.exceptions.client;
 public class ClientService {
 
     public static void searchClient(String code) throws ClientNotFoundException {
-        //BUSCA NO BANCO DE DADOS
-        boolean isSigned = false;
-
-        if (!isSigned) {
-            throw new ClientNotFoundException("Cliente não foi encontrado!");
-        }
+        ClientDAO.searchClient(code);
     }
 }
