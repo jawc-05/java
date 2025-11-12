4e53dd6a548d1b65a32727a10d648a5d7b9bf200
jawc-05 <joaoalfredowc@gmail.com>
2025-11-12 15:07:38 -0300

    feat: adding ExampleThrow class

--- br/com/jawc/exceptions/ExampleExceptionBasic.java
+++ br/com/jawc/exceptions/ExampleExceptionBasic.java
@@ -49,14 +49,5 @@ public class ExampleExceptionBasic {
         }
     }
 
-    private static void saque (double valor) throws LimiteSaqueException{
 
-        if (valor > 400){
-            LimiteSaqueException erro =
-                    new LimiteSaqueException("O valor solicitado passa o limite diário que pode ser sacado nessa conta");
-            throw erro;
-        }else{
-            System.out.println("Saldo retirado da conta: "+valor+"$");
-        }
-    }
 }
