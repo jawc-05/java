48f46d24d4c8158c971d03d459350d910466d2a4
jawc-05 <joaoalfredowc@gmail.com>
2026-03-04 14:55:50 +0100

    feat: adding getFields e Constructor in app

--- br/com/jawc/reflections/App.java
+++ br/com/jawc/reflections/App.java
@@ -3,6 +3,10 @@
  */
 package br.com.jawc.reflections;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+
 public class App {
 
     public static void main(String[] args) {
@@ -14,5 +18,23 @@ public class App {
         Class pClass2 = prod.getClass();
         System.out.println(pClass2);
 
+        try{
+            Constructor cons = pClass.getConstructor();
+            Produto prod1 = (Produto) cons.newInstance();
+            System.out.println(cons);
+            System.out.println(prod1);
+
+            Field[] fields = prod1.getClass().getDeclaredFields();
+            for (Field field : fields){
+                Class<?> typeF = field.getType();
+                String nameF = field.getName();
+                System.out.println(typeF);
+                System.out.println(nameF);
+            }
+
+
+        }catch(NoSuchMethodException|InvocationTargetException| InstantiationException| IllegalAccessException e){
+            e.printStackTrace();
+}
     }
 }
