036d1761600eb27b3e033789cd9c8a7e6ce2aded
jawc-05 <joaoalfredowc@gmail.com>
2026-03-06 16:32:54 +0100

    feat: adding the Produto class

---
+++ br/com/jawc/reflections/reflectionExercise/Produto.java
@@ -0,0 +1,13 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.reflections.reflectionExercise;
+
+import br.com.jawc.reflections.reflectionExercise.Tabela;
+
+@Tabela(name="product_tb", description = "A table where we storage the products", code = 911)
+public class Produto {
+
+    String name_product;
+    Long code_product;
+}
