99d7eafd964388fe1332e03a1c3a32e543be2d5a
jawc-05 <joaoalfredowc@gmail.com>
2026-03-06 16:53:19 +0100

    feat: added after debugging the exercise for Module19

--- br/com/jawc/reflections/reflectionExercise/Tabela.java
+++ br/com/jawc/reflections/reflectionExercise/Tabela.java
@@ -3,6 +3,10 @@
  */
 package br.com.jawc.reflections.reflectionExercise;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Tabela {
     String name();
     String description();
