8932882ce917e4a5e61ec29fcf5e1ee84a504482
jawc-05 <joaoalfredowc@gmail.com>
2026-03-10 18:50:46 +0100

    feat: adding simple example of Bifunc in  Lambda

--- br/com/jawc/lambda/TestsLambda.java
+++ br/com/jawc/lambda/TestsLambda.java
@@ -12,8 +12,14 @@ public class TestsLambda {
         IntBinaryOperator iBin = (int a, int b) -> {
             return a + b;
         };
+         System.out.println("IntBinaryOperator:");
          System.out.println( iBin.applyAsInt(1, 2));
 
-        BiFunction<Integer, Integer, Integer> biFunc = (a, b) -> a + b; 
+        BiFunction<Integer, Integer, Integer> biFunc = (a, b) -> {
+            System.out.println("BiFunction: ");
+            return a + b;
+        };
+        System.out.println(biFunc.apply(2, 2)); //Integer resultBi = biFunc.apply(2,2);
+                                                        //System.out.println(resultBi);
     }
 }
