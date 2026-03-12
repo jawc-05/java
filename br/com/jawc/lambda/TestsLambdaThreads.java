4467aba9a2a6e3bc161101f4d7a9c164fd45546f
jawc-05 <joaoalfredowc@gmail.com>
2026-03-12 20:31:51 +0100

    feat: adding to show difference in using Runnable with and without Lambda expressions

---
+++ br/com/jawc/lambda/TestsLambdaThreads.java
@@ -0,0 +1,23 @@
+/**
+ * @author jawc-05
+ */
+package br.com.jawc.lambda;
+
+public class TestsLambdaThreads {
+
+    public static void main(String[] args) {
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Executando Runnable de forma comum!");
+            }
+        };
+        new Thread(r).start();
+
+        Runnable r2 = ()->{
+            System.out.println("Executando Runnable com lambda!");
+        };
+        new Thread(r2).start();
+
+    }
+}
