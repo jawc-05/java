cdcce9a8d9d687f6f21c08d56400043e29c9e683
jawc-05 <joaoalfredowc@gmail.com>
2026-03-23 17:04:01 +0100

    feat: improved the filter example

--- br/com/jawc/streams/FilterExamples.java
+++ br/com/jawc/streams/FilterExamples.java
@@ -10,8 +10,17 @@ public class FilterExamples {
 
     public static void main(String[] args) {
 
-            List <Person> persons = new Person().createPersons();
-            Stream<Person> stream = persons.stream().filter(person -> person.getNationality().equals("Brazil"));
+            List <Person> list = new Person().createPersons();
+            Stream<Person> stream = list.stream()
+                    .filter(person -> person.getNationality().equals("Brazil"));
+            stream.forEach(System.out::println);
+
+
+            //In this example i used stream map, so now we can see the real String name of these persons
+        Stream<String> stream2 = list.stream()
+                .filter(person -> person.getNationality().equals("Brazil"))
+                .map(Person:: getName);
+        stream2.forEach(System.out::println);
         }
 
     }
