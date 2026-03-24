0f315f8d61da18565a537f94362d2ff596261cc5
jawc-05 <joaoalfredowc@gmail.com>
2026-03-24 14:21:24 +0100

    feat: adding LimitExamples

---
+++ br/com/jawc/streams/LimitExamples.java
@@ -0,0 +1,18 @@
+/**
+ * @author jawc-05
+ */
+package br.com.jawc.streams;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class LimitExamples {
+
+    public static void main(String[] args) {
+        System.out.println("LIMIT");
+
+        List<Person> list = new Person().createPersons();
+        Stream<Person> stream = list.stream().limit(2);
+        stream.forEach(System.out::println);
+    }
+}
