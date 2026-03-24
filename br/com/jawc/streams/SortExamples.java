188810e7d3996f1a8df5e669dd2775a7b29d0119
jawc-05 <joaoalfredowc@gmail.com>
2026-03-24 13:44:37 +0100

    feat: adding SortExamples class

---
+++ br/com/jawc/streams/SortExamples.java
@@ -0,0 +1,15 @@
+/**
+ * @author jawc-05
+ */
+package br.com.jawc.streams;
+
+import java.util.List;
+
+public class SortExamples {
+
+    public static void main(String[] args) {
+
+        List<Person> lis = new Person().createPersons();
+
+    }
+}
