1b31cc6005635548f9123e1cd1a7629205406cd5
jawc-05 <joaoalfredowc@gmail.com>
2026-03-24 15:13:01 +0100

    feat: adding the first example, it is the forEach one

--- br/com/jawc/streams/FinalOperationsWithStreams.java
+++ br/com/jawc/streams/FinalOperationsWithStreams.java
@@ -12,5 +12,11 @@ public class FinalOperationsWithStreams {
     public static void main(String[] args) {
         List<Person> list = new Person().createPersons();
 
+        forEach(list);
+    }
 
+    private static void forEach(List<Person> list) {
+        list.stream().forEach(person -> System.out.println(person.getName()));
+        list.forEach(person -> System.out.println(person.getName()));
+    }
 }
