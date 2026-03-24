6efb798f610e6bfcda796e056a487e668526ce27
jawc-05 <joaoalfredowc@gmail.com>
2026-03-24 15:12:39 +0100

    feat: adding the base for the examples in this class

--- br/com/jawc/streams/FinalOperationsWithStreams.java
+++ br/com/jawc/streams/FinalOperationsWithStreams.java
@@ -3,9 +3,14 @@
  */
 package br.com.jawc.streams;
 
+import br.com.jawc.streams.intermediates.Person;
+
+import java.util.List;
+
 public class FinalOperationsWithStreams {
 
     public static void main(String[] args) {
+        List<Person> list = new Person().createPersons();
 
-    }
+
 }
