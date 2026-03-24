6248da3b5ddbd1ffdcec583d18318e0255e9058f
jawc-05 <joaoalfredowc@gmail.com>
2026-03-24 15:20:36 +0100

    feat: improving the first example

--- br/com/jawc/streams/FinalOperationsWithStreams.java
+++ br/com/jawc/streams/FinalOperationsWithStreams.java
@@ -17,6 +17,13 @@ public class FinalOperationsWithStreams {
 
     private static void forEach(List<Person> list) {
         list.stream().forEach(person -> System.out.println(person.getName()));
-        list.forEach(person -> System.out.println(person.getName()));
+
+        System.out.println("********** BRASILEIROS **********");
+
+        list.stream()
+                .filter(person -> person.getNationality().equals("Brazil"))
+                /// .forEach(pBrazilian -> System.out.println(pBrazilian)) it'll work, but is gonna show the hashCodes not the "real" names
+                .forEach(pBrazilian -> System.out.println(pBrazilian.getName()));
+
     }
 }
