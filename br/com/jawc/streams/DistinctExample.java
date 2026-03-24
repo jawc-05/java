f50ee1e553cfa4a391015393b424b94df656fe21
jawc-05 <joaoalfredowc@gmail.com>
2026-03-24 14:07:09 +0100

    feat: addingte distinct example

---
+++ br/com/jawc/streams/DistinctExample.java
@@ -0,0 +1,15 @@
+/**
+ * @author jawc-05
+ */
+package br.com.jawc.streams;
+
+import java.util.List;
+
+public class DistinctExample {
+
+    public static void main(String[] args) {
+
+         List<Person> list = new Person().createPersons();
+
+    }
+}
