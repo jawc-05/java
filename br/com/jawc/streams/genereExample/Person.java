399177cfb93a418b6d51371e2eb4f01eeeea96c5
jawc-05 <joaoalfredowc@gmail.com>
2026-03-25 10:59:19 +0100

    feat: adding new pkg

---
+++ br/com/jawc/streams/genereExample/Person.java
@@ -0,0 +1,26 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc.streams.genereExample;
+
+public class Person {
+
+    private String name;
+    private String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
