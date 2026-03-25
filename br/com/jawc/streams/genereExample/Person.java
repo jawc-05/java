4a9040df888c73a682be281da240d9c082df2a36
jawc-05 <joaoalfredowc@gmail.com>
2026-03-25 11:04:19 +0100

    feat: adding the list of Persons

--- br/com/jawc/streams/genereExample/Person.java
+++ br/com/jawc/streams/genereExample/Person.java
@@ -3,10 +3,15 @@
  */
 package br.com.jawc.streams.genereExample;
 
+import java.util.List;
+
 public class Person {
 
     private String name;
     private String sex;
+
+    public Person(String name, String sex) {
+    }
 
     public String getName() {
         return name;
@@ -22,5 +27,15 @@ public class Person {
 
     public void setSex(String sex) {
         this.sex = sex;
+    }
+
+    public List addingPersons(){
+        Person p1 = new Person("Maria", "Mulher");
+        Person p2 = new Person("Juan", "Homem");
+        Person p3 = new Person("Julia", "Mulher");
+        Person p4 = new Person("Cecília", "Mulher");
+        Person p5 = new Person("Pedro", "Homem");
+        Person p6 = new Person("Zoe", "Mulher");
+        return List.of(p1,p2,p3,p4,p5,p6);
     }
 }
