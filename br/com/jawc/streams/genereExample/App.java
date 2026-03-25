eead911205645b732f5ed895af434b9165a52d71
jawc-05 <joaoalfredowc@gmail.com>
2026-03-25 11:16:44 +0100

    feat: adding the exercise

--- br/com/jawc/streams/genereExample/App.java
+++ br/com/jawc/streams/genereExample/App.java
@@ -4,6 +4,7 @@
 package br.com.jawc.streams.genereExample;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class App {
 
@@ -13,6 +14,17 @@ public class App {
         list.stream()
                 .forEach(System.out::println);
 
+        System.out.println("***** LISTA COM APENAS AS MULHERES *****");
+        List<Person> mulheres = list.stream()
+                .filter(person -> person.getSex().equals("Mulher"))
+                .collect(Collectors.toList());
+        mulheres.forEach(System.out::println);
+
+        System.out.println("***** LISTA COM APENAS OS HOMEMS *****");
+        List<Person> homens = list.stream()
+                .filter(person -> person.getSex().equals("Homem"))
+                .collect(Collectors.toList());
+        homens.forEach(System.out::println);
 
     }
 }
