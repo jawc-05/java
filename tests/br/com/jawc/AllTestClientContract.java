0ff45143519e0a3403c845fb8f0c1f06e7eb8947
jawc-05 <joaoalfredowc@gmail.com>
2026-03-27 20:23:05 +0100

    feat: addin allTest class

---
+++ tests/br/com/jawc/AllTestClientContract.java
@@ -0,0 +1,12 @@
+/**
+ * @author jawc
+ */
+package br.com.jawc;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ClienteServiceTest.class, ContractServiceTest.class})
+public class AllTestClientContract {
+}
