3b9835798a6e86293237ee498b048ae26863ad31
jawc-05 <joaoalfredowc@gmail.com>
2026-03-27 13:05:04 +0100

    feat: adding basic stuff to test in client

--- src/br/com/jawc/mock/dao/ClienteDao.java
+++ src/br/com/jawc/mock/dao/ClienteDao.java
@@ -4,4 +4,8 @@
 package br.com.jawc.mock.dao;
 
 public class ClienteDao {
+
+    public String salvar(){
+        return "Sucesso!";
+    }
 }
--- src/br/com/jawc/mock/service/ClienteService.java
+++ src/br/com/jawc/mock/service/ClienteService.java
@@ -3,5 +3,18 @@
  */
 package br.com.jawc.mock.service;
 
+import br.com.jawc.mock.dao.ClienteDao;
+
 public class ClienteService {
+
+    private ClienteDao clienteDao;
+
+    public ClienteService(){
+        clienteDao = new ClienteDao();
+    }
+
+    public String salvar(){
+        clienteDao.salvar();
+        return "Sucesso!";
+    }
 }
